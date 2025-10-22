dad87c8913835fd3382a9bc7b51767334b63871b
Evandro Junior <khaljc22@proton.me>
2025-10-22 12:17:56 -0300

    80 - inserir conta com mesmo nome 82 - campos obrigatórios na movimentação 83 - movimentação futura 84 - remover movimentação

--- src/main/java/Page/ContasPage.java
+++ src/main/java/Page/ContasPage.java
@@ -5,15 +5,13 @@ import org.openqa.selenium.By;
 import core.BasePage;
 
 
-
-
 public class ContasPage extends BasePage {
 
     public void setNome(String nome) {
         escrever("nome", nome);
     }
 
-    public void salvar(){
+    public void salvar() {
         clicarBotao(By.xpath("//button[.='Salvar']"));
     }
 
@@ -21,14 +19,17 @@ public class ContasPage extends BasePage {
         return obterTexto(By.xpath("//div[@class='alert alert-success']"));
     }
 
-    public String obterMensagemErro(){
+    public String obterMensagemErro() {
         return obterTexto(By.xpath("//div[@class='alert alert-danger']"));
     }
 
     public void clicarAlterarConta(String string) {
         obterCelula("Conta", string, "Ações", "tabelaContas")
                 .findElement(By.xpath(".//span[@class='glyphicon glyphicon-edit']")).click();
-
-
+    }
+    public void clicarExcluirConta(String string) {
+        obterCelula("Conta", string, "Ações", "tabelaContas")
+                .findElement(By.xpath(".//span[@class='glyphicon glyphicon-remove-circle']")).click();
     }
 }
+
--- src/main/java/tests/RemoverMovimentacaoContaTest.java
+++ src/main/java/tests/RemoverMovimentacaoContaTest.java
@@ -1,4 +1,27 @@
 package tests;
 
-public class RemoverMovimentacaoContaTest {
-}
+import Page.MenuPage;
+import core.BaseTest;
+import core.Propriedades;
+import org.junit.Assert;
+import org.junit.Test;
+
+import Page.ContasPage;
+
+
+
+    public class RemoverMovimentacaoContaTest extends BaseTest {
+
+        MenuPage menuPage = new MenuPage();
+        ContasPage contasPage = new ContasPage();
+
+        @Test
+        public void testExcluirContaComMovimentacao(){
+            menuPage.acessarTelaListarConta();
+
+            contasPage.clicarExcluirConta(Propriedades.NOME_CONTA_ALTERADA);
+
+            Assert.assertEquals("Conta em uso na movimentações", contasPage.obterMensagemErro());
+        }
+    }
+
